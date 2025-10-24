91e05009c55a79a68c7340dc8e97321f7622ab03
UmangAmongUs <umang8108@gmail.com>
2025-10-24 20:21:56 +0530

    bug fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import BgSlider from '../components/BgSlider'
-import Testimonial from '../components/testimonial'
+import Testimonial from '../components/Testimonial'
 import Upload from '../components/Upload'
 
 const Home = () => {

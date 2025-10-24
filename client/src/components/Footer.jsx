3ad124f6ce46673d263a4e6792836fa4b73c5f6c
UmangAmongUs <umang8108@gmail.com>
2025-10-24 21:54:00 +0530

    footer button added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,9 +7,15 @@ const Footer = () => {
       <img className='hover:scale-110' width={150} src={assets.logo} alt="" />
       <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max:sm-hidden hover:underline'>Copyright @UmangAmongUs | All Right Reserved</p>
       <div className='flex gap-1'>
-        <img  className='hover:scale-110' width={40} src={assets.facebook_icon} alt="" />
-        <img  className='hover:scale-110' width={40} src={assets.twitter_icon} alt="" />
-        <img  className='hover:scale-110' width={40} src={assets.google_plus_icon} alt="" />
+        <a href = 'https://www.facebook.com' target='_blank' rel='noopener noreferrer'>
+          <img  className='hover:scale-110' width={40} src={assets.facebook_icon} alt="" />
+        </a>
+        <a href = 'https://x.com' target='_blank' rel='noopener noreferrer'>
+          <img  className='hover:scale-110' width={40} src={assets.twitter_icon} alt="" />
+        </a>
+        <a href = 'https://mail.google.com/mail/u/0/#inbox' target='_blank' rel='noopener noreferrer'>
+          <img  className='hover:scale-110' width={40} src={assets.google_plus_icon} alt="" />
+        </a>        
       </div>
     </div>
   )
